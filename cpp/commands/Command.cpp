520eb2cb115174a82f834518ec89daa6cc21ff15
gideon-del <gideonchidi471@gmail.com>
2026-02-12 00:44:31 +0100

    feat: update node command with fixes

--- cpp/commands/Command.cpp
+++ cpp/commands/Command.cpp
@@ -23,6 +23,8 @@ namespace CanvasMVP
 
     void RemoveNodeComand::execute(SceneGraph &sceneGraph)
     {
+        if (executed_)
+            return;
         auto node = sceneGraph.getNode(nodeId);
         if (node == nullptr)
         {
@@ -32,12 +34,59 @@ namespace CanvasMVP
         removedNode_ = std::make_unique<NodeSnapShot>(
             NodeSnapShot::fromNode(*node));
         sceneGraph.removeNode(nodeId);
+        executed_ = true;
     }
     void RemoveNodeComand::undo(SceneGraph &sceneGraph)
     {
+        if (!executed_)
+            return;
         if (removedNode_ == nullptr)
             return;
         sceneGraph.addNode(std::make_unique<Node>(removedNode_->toNode()));
+        executed_ = false;
+    }
+
+    UpdateNodeCommand::UpdateNodeCommand(NodeSnapShot updatedNode) : newState_(updatedNode) {};
+
+    void UpdateNodeCommand::execute(SceneGraph &sceneGraph)
+    {
+        auto node = sceneGraph.getNode(newState_.data.id);
+        if (node == nullptr)
+            return;
+        if (!hasOldState)
+        {
+            oldState_ = NodeSnapShot::fromNode(*node);
+            hasOldState = true;
         }
+        applyState(&sceneGraph, newState_);
+    }
+    void UpdateNodeCommand::applyState(SceneGraph *sceneGraph, NodeSnapShot &state)
+    {
+        auto node = sceneGraph->getNode(state.data.id);
+        if (node == nullptr)
+            return;
+        node->data = state.data;
+        sceneGraph->updateIndex(node);
+    }
+    void UpdateNodeCommand::undo(SceneGraph &sceneGraph)
+    {
+        if (!hasOldState)
+            return;
+        applyState(&sceneGraph, oldState_);
+    }
+    bool UpdateNodeCommand::canMergeWith(Command *other) const
+    {
+        auto otherUpdateCmd = dynamic_cast<UpdateNodeCommand *>(other);
+        if (!otherUpdateCmd)
+            return false;
+        return otherUpdateCmd->newState_.data.id == newState_.data.id;
+    }
+    void UpdateNodeCommand::mergeWith(Command *other)
+    {
+        auto otherUpdateCmd = dynamic_cast<UpdateNodeCommand *>(other);
+        if (!otherUpdateCmd)
+            return;
+        newState_ = otherUpdateCmd->newState_;
+    }
 
 }
--- cpp/commands/Command.h
+++ cpp/commands/Command.h
@@ -7,8 +7,8 @@ namespace CanvasMVP
     {
     public:
         virtual ~Command() = default;
-        virtual void execute(SceneGraph &sceneGraph);
-        virtual void undo(SceneGraph &sceneGraph);
+        virtual void execute(SceneGraph &sceneGraph) = 0;
+        virtual void undo(SceneGraph &sceneGraph) = 0;
         virtual bool canMergeWith(Command *other) const { return false; };
         virtual void mergeWith(Command *other) {}
     };
@@ -30,11 +30,28 @@ namespace CanvasMVP
     private:
         std::unique_ptr<NodeSnapShot> removedNode_;
         std::string nodeId;
+        bool executed_ = false;
 
     public:
         RemoveNodeComand(std::string nodeId);
         void execute(SceneGraph &sceneGraph) override;
         void undo(SceneGraph &sceneGraph) override;
+    };
+
+    class UpdateNodeCommand : public Command
+    {
+    private:
+        NodeSnapShot oldState_;
+        NodeSnapShot newState_;
+        bool hasOldState = false;
+        void applyState(SceneGraph *sceneGraph, NodeSnapShot &state);
+
+    public:
+        UpdateNodeCommand(NodeSnapShot newNode);
+        void execute(SceneGraph &sceneGraph) override;
+        void undo(SceneGraph &sceneGraph) override;
+        bool canMergeWith(Command *other) const override;
+        void mergeWith(Command *other) override;
     };
 
 }
